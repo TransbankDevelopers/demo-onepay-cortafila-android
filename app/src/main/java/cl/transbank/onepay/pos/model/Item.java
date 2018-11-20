39b6b28e31e2a43cbf0e8b2be0b222b8f542588c
Rodrigo Ayala <rayalac@gmail.com>
2018-11-20 19:26:00 -0300

    Show payment flow without webservice integration

---
+++ app/src/main/java/cl/transbank/onepay/pos/model/Item.java
@@ -0,0 +1,66 @@
+package cl.transbank.onepay.pos.model;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Item implements Parcelable {
+    private String description;
+    private Integer quantity;
+    private Integer amount;
+    private String additionalData;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getAdditionalData() {
+        return additionalData;
+    }
+
+    public void setAdditionalData(String additionalData) {
+        this.additionalData = additionalData;
+    }
+
+    public Integer getExpire() {
+        return expire;
+    }
+
+    public void setExpire(Integer expire) {
+        this.expire = expire;
+    }
+
+    private Integer expire;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(description);
+        dest.writeInt(quantity);
+        dest.writeInt(amount);
+        dest.writeString(additionalData);
+    }
+}
